a5aa96655a2ab051a9623a7d59d2392ce011a585
zekun-wu <wuzekun@cs.uni-saarland.de>
2023-02-25 00:11:59 +0100

    Updates

---
+++ precache-manifest.be69deaf99c790af6bf72c25d3629d6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a18f073dff293f02bcb90e9a8c59fe64",
+    "url": "/page/index.html"
+  },
+  {
+    "revision": "3f3f9fcee6d689c0e7a6",
+    "url": "/page/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "fbca831f7afdf6589469",
+    "url": "/page/static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "3f3f9fcee6d689c0e7a6",
+    "url": "/page/static/js/2.6e9da6bc.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/page/static/js/2.6e9da6bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbca831f7afdf6589469",
+    "url": "/page/static/js/main.1c5ceddb.chunk.js"
+  },
+  {
+    "revision": "77d514e5a18ad4fe841f",
+    "url": "/page/static/js/runtime-main.0cc903ba.js"
+  }
+]);
